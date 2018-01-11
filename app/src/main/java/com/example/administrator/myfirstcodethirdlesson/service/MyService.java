49371fb88fba7dfd1169b4eac6a3a15995853476
leichen <18829026834@163.com>
2018-01-11 17:45:40 +0800

    api less 16

--- app/src/main/java/com/example/administrator/myfirstcodethirdlesson/service/MyService.java
+++ app/src/main/java/com/example/administrator/myfirstcodethirdlesson/service/MyService.java
@@ -5,7 +5,9 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -38,13 +40,19 @@ public class MyService extends Service{
 
         Intent intent = new Intent(this,MyServiceActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
-
-        Notification.Builder builder = new Notification.Builder(this);
-        Notification notification = builder.setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setWhen(System.currentTimeMillis()).setContentTitle("Notitle").setContentText("is coming").build();
-        startForeground(1,notification);
-
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.JELLY_BEAN){
+            Notification.Builder builder = new Notification.Builder(this);
+            Notification notification = builder.setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.weclome)
+                    .setWhen(System.currentTimeMillis()).setContentTitle("Notitle").setContentText("is coming").build();
+            startForeground(1, notification);
+        }
+        else {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+            Notification notificationCompat = builder.setSmallIcon(R.drawable.weclome)
+                    .setContentIntent(pendingIntent).setWhen(System.currentTimeMillis()).setContentTitle("lkk").setContentText("we").build();
+            startForeground(2,notificationCompat);
+        }
         Log.e("@lei","mcreate");
     }
 
